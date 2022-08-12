b377997e43bac8219c91bc85dec226053119f4d9
Davi S. Bento <contato@davisbento.dev>
2022-08-12 19:40:04 -0300

    feat: signup endpoint

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
 # sync prisma schema to database
 update-db:
 	npx prisma db push
+
+# bring db changes to the codebase/prisma schemas
+update-code:
+	npx prisma db pull
+
+create-account:
+	curl -X POST \
+		-H "Content-Type: application/json" \
+		-d '{"email": "test@test.com", "password": "test"}' \
+		http://localhost:3000/api/signup
---
+++ pages/api/signup.ts
@@ -0,0 +1,60 @@
+import Cookies from 'cookies';
+
+import { generateToken } from '../../libs/token';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const methodsAllowed = ['POST'];
+
+	if (!methodsAllowed.includes(req?.method || '')) {
+		res.status(405).json({ message: 'Method not allowed' });
+		return;
+	}
+
+	const cookies = new Cookies(req, res);
+
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		res.status(401).json({ message: 'No email or password' });
+		return;
+	}
+
+	// create user using the pruisma client
+	const user = await prisma.user.findFirst({
+		where: {
+			email
+		}
+	});
+
+	if (user) {
+		res.status(401).json({ message: 'User already exists' });
+		return;
+	}
+
+	await prisma.user.create({
+		data: {
+			email,
+			password
+		}
+	});
+
+	const authToken = generateToken();
+
+	// Set the authToken as an HTTP-only cookie.
+	// We'll also set the SameSite attribute to
+	// 'lax' for some additional CSRF protection.
+	cookies.set('auth-token', authToken, {
+		httpOnly: true,
+		sameSite: 'lax'
+	});
+
+	// Our response to the client won't contain
+	// the actual authToken. This way the auth token
+	// never gets exposed to the client.
+	res.status(200).json({ loggedIn: true });
+}
