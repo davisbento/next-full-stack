87b42c9da188e8619412815ab3213f580ce1ba93
Davi S. Bento <contato@davisbento.dev>
2022-08-15 12:31:49 -0300

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleDirectories: ['node_modules', 'src'],
+	globals: {
+		'ts-jest': {
+			isolatedModules: true
+		}
+	}
 };
--- services/auth.spec.ts
+++ services/auth.spec.ts
@@ -1,3 +1,5 @@
-test('adds 1 + 2 to equal 3', () => {
-	expect(true).toBe(true);
+describe('AuthService', () => {
+	test('adds 1 + 2 to equal 3', () => {
+		expect(true).toBe(true);
+	});
 });
